be2dd580ea542f00b0506e4436b3a7c539f1dd5c
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2023-12-03 12:12:27 +0000

    ✨ Playing with writeable signals creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,29 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'angular-signals-course';
   value = signal(15);
+  counter = signal(0);
+
+  constructor(){
+    setTimeout(() => {
+      // Replacing the initial value of 15 with 30 after 1000ms.
+        this.value.set(30);
+    }, 1000);
+
+    setInterval(() => {
+      // Increment the value by one, every 1000ms.
+      this.value.update(v => v + 1);
+    }, 2000);
+  }
+
+  reset() {
+    this.counter.set(0);
+  }
+
+  add() {
+    this.counter.update(c => c + 1);
+  }
+
+  multiply() {
+    this.counter.update(c => c * 2);
+  }
 }
